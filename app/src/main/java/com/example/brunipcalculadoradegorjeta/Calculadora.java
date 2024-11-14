ab4724cae8ec43bb16193667df5878bcf2d01299
Tiago061 <Thenriqueleite277@gmail.com>
2024-11-13 23:51:32 -0300

    Calculadora de gorjeta criada, layout criado, classe calculadora implementada

---
+++ app/src/main/java/com/example/brunipcalculadoradegorjeta/Calculadora.java
@@ -0,0 +1,16 @@
+package com.example.brunipcalculadoradegorjeta;
+
+public class Calculadora {
+
+    static double gorjeta(double valor, double percentual){
+        return valor * percentual / 100.0;
+    }
+
+    static double [] gorjeta(double valor){
+        double[] saida = new double[3];
+        for(int i = 0; i < 3; i++){
+            saida[i] = gorjeta(valor, i * 5 + 5);
+        }
+        return saida;
+    }
+}
